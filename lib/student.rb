5d28def23eb31214312e21a8312b154b04807b6f
German Cruz <cruzgerman216@yahoo.com>
2020-04-25 16:05:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,8 +53,8 @@ class Student
     end
 
     def self.new_from_db(row)
-      student = self.create(row[1],row[2])
-      student
+      student_new = self.new(row[0], row[1], row[2])
+  student_new
     end
 
     def self.find_by_name(name)
