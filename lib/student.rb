e5f9951c005ecf5e3daf34a66c2bcd5ffd197aac
German Cruz <cruzgerman216@yahoo.com>
2020-04-25 16:11:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,9 +65,7 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql,name).map do |row|
-      self.new_from_db(row)
-    end.first
-
+    get = DB[:conn].execute(sql,name)[0]
+      Student.new(get[0],get[1], get[2])
     end
 end
